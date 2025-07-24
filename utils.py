0f190296fcc7bce0f5b6e90070af3c4ae8c059bf
Shaleyna09 <shaleynarungasamy@gmail.com>
2025-07-24 18:25:31 +0400

    vbnmk,l;'

--- utils.py
+++ utils.py
@@ -1,10 +1,7 @@
-import random
-
-
 def get_random_word():
-    with open('wordlist.txt', 'r') as file:
-        text = file.read()
-    words = text.split()
-    random_word = random.choice(words)
-    print(random_word)
-get_random_word()
+    import random
+    with open("wordlist.txt") as list:
+        text = list.read().strip()
+    word = text.split()
+    random_word = random.choice(word)
+    return random_word
