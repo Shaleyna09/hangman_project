13a809d2f0dcaeaaba68a0475fd545e5d382a707
Shaleyna09 <shaleynarungasamy@gmail.com>
2025-07-24 18:16:44 +0400

    asdfghjkl

--- game.py
+++ game.py
@@ -4,12 +4,14 @@ class HangmanGame:
         self.guessed_letters = guessed_letters
         self.max_errors = max_errors
         self.errors = errors
+   
     def display_word(self):
         for letter in self.word:
             if letter in self.guessed_letters:
                 print(letter, end=' ')
             else:
                 print('_', end=' ')
+    
     def process_guess(self, letter):
         if letter in self.word:
             self.guessed_letters.append(letter)
@@ -17,3 +19,21 @@ class HangmanGame:
         else:
             self.errors += 1
             return False
+    
+    def is_won(self):
+        return all(letter in self.guessed_letters for letter in self._word)
+    
+    def is_lost(self):
+        tries_left = self.max_errors - self.errors
+        print(f"You have {tries_left} tries left.")
+    
+    def display_progress(self):
+        display = ""
+        for char in self.word:
+            if not char.isalpha():
+                display += char + " "
+            elif char in self.guessed_letters:
+                display += char + " "
+            else:
+                display += "_ "
+        return display.strip()
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from game import HangmanGame
 previous_inputs = set()
 while True:
     Kartoffel = input("Enter a letter: ")
-    if not (Kartoffel.isalpha() and len(Kartoffel) == 1):
+    if not Kartoffel.isalpha():
+        print("It must be a letter.")
+        continue
+
+    elif not len(Kartoffel) == 1:
         print("Please enter a single letter.")
         continue
-    if Kartoffel in previous_inputs:
+
+    elif Kartoffel in previous_inputs:
         print("You have already guessed that letter. Try again.")
+
+    elif previous_inputs.add(Kartoffel):
+        print(f"You guessed: {previous_inputs}")
+    
     else:
         previous_inputs.add(Kartoffel)
-        print(f"You guessed: {Kartoffel}")
+        print(f"You entered: {Kartoffel}")
+        print(f"You guessed: {previous_inputs}")
