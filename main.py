cbfaa36d6d5ad8886aff92c71481188a1c9dc39a
Shaleyna09 <shaleynarungasamy@gmail.com>
2025-07-24 20:45:05 +0400

    dfghjk

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 from game import HangmanGame
-game = HangmanGame
-ze_word =  set()
+
+game = HangmanGame()
+ze_game = set()
+
 while True:
-    Kartoffel = input("Enter a letter: ")
-    if not Kartoffel.isalpha():
-        print("It must be a letter.")
+    kartoffel = input('Enter a letter: ')
+    if not len(kartoffel) == 1:
+        print('It must be a single character.')
         continue
-
-    elif not len(Kartoffel) == 1:
-        print("Please enter a single letter.")
+    elif not kartoffel.isalpha():
+        print('It must be a letter')
         continue
-
-    elif Kartoffel in ze_word:
-        print("You have already guessed that letter. Try again.")
-
-    elif ze_word.add(Kartoffel):
-        print(f"You guessed: {ze_word}")
+    elif kartoffel in ze_game:
+        print("You've already tried this letter. Try again.")
     else: 
-        ze_word.add(Kartoffel)
-        game.guess(Kartoffel)
+        ze_game.add(kartoffel)
+        game.guess(kartoffel)
 
     if game.is_won():
         print(f"You won! The word was: {game.word}")
